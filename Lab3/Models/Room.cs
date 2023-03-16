469cd9a0f287deaaae651adfd719b3b4170fc934
Lei Sun <139188566@qq.com>
2023-03-16 16:16:39 -0500

    Create

---
+++ Lab3/Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Room
+    {
+        [Key]
+        public int RoomNumber { get; set; }
+        [Required]
+        [Range(1,6)]
+        public int  Capacity { get; set; }
+
+        public Client? Client { get; set; }
+        
+
+    }
+}
